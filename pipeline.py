81ecb8a9742e8bc164a25c803dba37b1ea8e7229
taotao <tao.tao@mitigenomics.com>
2018-08-17 21:46:48 +0800

    update

--- pipeline.py
+++ pipeline.py
@@ -40,7 +40,7 @@ def mkdirs(*paths):
 
 def write_to_csv(csv, *lst):
     if lst:
-        lst = [ i if len(i) else "" for i in lst  ]
+        lst = [ i if len(i) > 0 else "" for i in lst  ]
         line = ",".join(lst)
         os.system("echo %s >> %s" % (line, csv))
 
@@ -67,39 +67,39 @@ class Pipeline(object):
         self.pool      = Pool(self.sync_cnt)
         self.pool.terminate()
         if self.run_csv:
-            if not os.path.exists(self.run_csv):
-                os.system("echo 'id,procedure,target,start_time,end_time,cost_time' > %s" % self.run_csv)
-            else:
+            if os.path.exists(self.run_csv):
                 lines = read_csv(self.run_csv)
                 # skip header
                 lines = lines[1:]
                 for line in lines:
-                    id, procedure, target, start_time, end_time, cost_time = line
-                    index = "%s:%s:%s" % (id, procedure, target)
+                    ID, procedure, target, start_time, end_time, cost_time = line
+                    index = "%s:%s:%s" % (ID, procedure, target)
                     self.run_array[index] = "%s %s %s" % (start_time, end_time, cost_time)
+            else:
+                os.system("echo 'ID,procedure,target,start_time,end_time,cost_time' > %s" % self.run_csv)
 
-    def append(self, id, procedure, cmd, target = None, log = None, run_sync = 0):
+    def append(self, ID, procedure, cmd, target = None, log = None, run_sync = 0):
         # 有些procedure, 是'无视'target,可以并行跑
         # 如在一个procedure里，没有指明target，或者指向一致的情况下本来就是并行的
         # 增加run_sync, 可以在target不一致的情况下,并行运行.
         # 通过构建index的方式, 一个pipeline是一个list,可以并行执行
         if run_sync:
-            index = "%s:%s:" % (id, procedure)
+            index = "%s:%s:" % (ID, procedure)
         else:
             if not len(target):
-                index = "%s:%s:" % (id, procedure)
+                index = "%s:%s:" % (ID, procedure)
             else:
-                index = "%s:%s:%s" % (id, procedure, target)
+                index = "%s:%s:%s" % (ID, procedure, target)
         if self.pipelines.get(index, None):
-            self.pipelines[index].append([id, procedure, cmd, target, log])
+            self.pipelines[index].append([ID, procedure, cmd, target, log])
         else:
-            self.pipelines[index] = [[id, procedure, cmd, target, log]]
+            self.pipelines[index] = [[ID, procedure, cmd, target, log]]
 
     def print_pipeline(self):
         for index in self.pipelines:
             pipeline = self.pipelines[index]
             for each in pipeline:
-                id, procedure, cmd, target, log = each
+                ID, procedure, cmd, target, log = each
                 print("===============================\n%s\n\n%s" % (index, cmd))
 
     def run_pipeline(self):
@@ -111,20 +111,20 @@ class Pipeline(object):
                 sync_cnt = self.sync_cnt
             self.pool = Pool(sync_cnt, init_worker, maxtasksperchild = sync_cnt)
             for each in pipeline:
-                id, procedure, cmd, target, log = each
-                self.pool.apply_async(Pipeline.run_cmd, args = (id, procedure, cmd, target, log, self.test, self.run_array, self.run_csv))
+                ID, procedure, cmd, target, log = each
+                self.pool.apply_async(Pipeline.run_cmd, args = (ID, procedure, cmd, target, log, self.test, self.run_array, self.run_csv))
             self.pool.close()
             self.pool.join()
             self.pool.terminate()
 
     @staticmethod
-    def run_cmd(id, procedure, cmd, target, log, test, run_array, run_csv):
+    def run_cmd(ID, procedure, cmd, target, log, test, run_array, run_csv):
         try:
             start_time = datetime.datetime.now()
             now        = start_time.strftime("%Y-%m-%d %H:%M:%S")
             if not len(target):
                 target = ""
-            ruuned     = "%s:%s:%s" % (id, procedure, target)
+            ruuned     = "%s:%s:%s" % (ID, procedure, target)
             if ruuned in run_array:
                 pass
             else:
@@ -141,7 +141,7 @@ class Pipeline(object):
                     start_time_reform = start_time.strftime("%Y-%m-%d %H:%M:%S")
                     end_time_reform   = end_time.strftime("%Y-%m-%d %H:%M:%S")
                     if run_csv:
-                        write_to_csv(run_csv, id, procedure, target, start_time_reform, end_time_reform, cost_time_reform)
+                        write_to_csv(run_csv, ID, procedure, target, start_time_reform, end_time_reform, cost_time_reform)
                     print(index, "finished at", end_time_reform )
         except subprocess.CalledProcessError as e:
             end_time = datetime.datetime.now()
